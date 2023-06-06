78a253a78f0f5f32e808714be28c9f39be405562
Aiden5486Pearce <60283775+Aiden5486Pearce@users.noreply.github.com>
2023-06-06 20:20:30 +0530

    reactChatApp

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBJs_E7MxTMOngoxtzfl6rq1FGy04-luC4",
+  authDomain: "aidenpearce-chat-app.firebaseapp.com",
+  projectId: "aidenpearce-chat-app",
+  storageBucket: "aidenpearce-chat-app.appspot.com",
+  messagingSenderId: "203688073463",
+  appId: "1:203688073463:web:83bc584c0cc7731d54dbf2"
+};
+ export const app = initializeApp(firebaseConfig);
